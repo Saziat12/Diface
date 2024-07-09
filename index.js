5e4db0229c55e1a523de9fea3416c54c20229416
Saziat12 <157962211+Saziat12@users.noreply.github.com>
2024-07-09 20:46:17 +0600

    Try to do something new.
    Pray for me.

---
+++ index.js
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked by-[SRH]</title>
+    <style>
+        body {
+    font-family: 'Courier New', Courier, monospace;
+    background-color: black;
+    color: #00ff00;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    overflow: hidden;
+    flex-direction: column;
+}
+
+#container {
+    text-align: center;
+    display: flex;
+}
+
+#container img {
+    width: 200px;
+    filter: blur(0px);
+}
+
+#container .first-img {
+    margin-right: 40px;
+}
+
+#container .second-img {
+    margin-left: 40px;
+} 
+
+h1 {
+    color: #00ff00;
+    font-size: 40px;
+    animation: blink 1s infinite;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: #00ff00;
+    color: black;
+    border: none;
+    border-radius: 4px;
+    margin-top: 20px;
+}
+
+.binary-digit {
+    position: absolute;
+    color: #00ff00;
+    font-size: 12px;
+    white-space: nowrap;
+    opacity: 0.6;
+    user-select: none;
+    animation: falling 5s linear infinite;
+}
+
+.container-text {
+    color: #00ff00;
+    text-align: center;
+}
+
+@keyframes blink {
+    0%, 100% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+}
+
+@keyframes falling {
+    to {
+        transform: translateY(100vh);
+    }
+}
+
+/* Media query for smaller screens */
+
+@media only screen and (max-width: 600px) {
+    body {
+        font-size: 16px;
+    }
+
+    h1 {
+        font-size: 24px;
+    }
+
+    #container img {
+        width: 100px;
+    }
+
+    .container-text {
+        font-size: 14px;
+    }
+}
+    </style>
+</head>
+<body>
+
+    <div id="binary-rain"></div>
+    <div id="container">
+        <img src="https://i.ibb.co/r05Hm2R/bmgl.png" class="first-img">
+        <h1>Hacked by-[SRH]</h1>
+        <img src="https://i.ibb.co/rk9DjT6/2u.png" class="second-img">
+    </div>
+
+    <div class="container-text">
+        <h2>Bangladeshi Muslim Black Hat Hacker#</h2>
+        <h2>Security is a Joke</h2>
+        <h2>[#]Thanks TO [#]</h2>
+    </div>
+
+    <button id="back-to-home">BACK TO HOME</button>
+
+    <script>
+function createBinaryRain() {
+    var binaryRain = document.getElementById("binary-rain");
+    var binaryPattern = ["10", "001", "00", "11", "0", "1", "100", "0001", "0000", "1000", "1111"];
+
+    for (var i = 0; i < 50; i++) { 
+        var binaryDigit = document.createElement("span");
+        binaryDigit.className = "binary-digit";
+        binaryDigit.innerHTML = binaryPattern[i % binaryPattern.length];
+        binaryDigit.style.position = "absolute";
+        binaryDigit.style.left = Math.random() * 100 + "vw";
+        binaryDigit.style.top = "-10vh"; // Start from the top
+        binaryDigit.style.animationDelay = (i * 0.1) + "s";
+        binaryRain.appendChild(binaryDigit);
+    }
+}
+
+createBinaryRain();
+
+// Add event listener to the button
+document.getElementById("back-to-home").addEventListener("click", function() {
+    alert("YOU ARE NOOB..!");
+    location.reload();
+});
+</script>
+
+<script type="text/javascript">
+function disableselect(e) {
+return false
+}
+function reEnable() {
+return true
+}
+document.onselectstart = new Function("return false")
+if (window.sidebar) {
+document.onmousedown = disableselect
+document.onclick = reEnable
+}
+var message = "";
+function clickIE() {
+if (document.all) {
+(message);
+return false;
+}
+}
+function clickNS(e) {
+if (document.layers || (document.getElementById && !document.all)) {
+if (e.which == 2 || e.which == 3) {
+(message);
+return false;
+}
+}
+}
+if (document.layers) {
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown = clickNS;
+} else {
+document.onmouseup = clickNS;
+document.oncontextmenu = clickIE;
+}
+document.oncontextmenu = new Function('alert("~/ Do Not Try /~"); return false')	
+</script>
+<center><img src="https://nathanprinsley-files.prinsh.com/data-1/images/NathanPrinsley-yaTAF.gif" 
+
+height="7" width="502"><br><img src="https://nathanprinsley-files.prinsh.com/data-1/images/NathanPrinsley-AqMFG.gif" 
+
+height="17" width="209"></center>
+</span></center></font>
+<font color="#B6900C" size="1.5" face="Tahoma" style="margin: 0px; padding: 0px">
+<b><u>! WARNING !</u></br>
+<font face="Iceland" size="2" color="red" style="text-shadow:0 0 25px" ALIGN="Bottom">Patch your Website</br> </font><img src="https://nathanprinsley-files.prinsh.com/data-1/images/NathanPrinsley-emoticon-0109-kiss.gif"><p>
+</center></br>
+
+<audio src="https://dl.sndup.net/r2sw/joy-sonar-bangla-song.mp3" loop="1" autoplay="1"></audio>
+
+    
+</body>
+</html>
